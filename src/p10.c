a835631600a2e11dc5f2e52c32a03237cf9abf19
Tianle Yu <me@tianleyu.com>
2026-05-23 20:30:09 -0700

    [feat] text headr

--- src/p10.c
+++ src/p10.c
@@ -71,7 +71,7 @@ void SPI_RENDER(uint8_t *frame)
                      : "r"(0)
                      : "memory");
         gpio_set(OE, 1);
-        sleep_us(10);
+        sleep_us(1);
         gpio_set(OE, 0);
     }
 }
@@ -107,6 +107,76 @@ uint32_t frame_time;
 uint8_t frame_buffer[ARRAY_FRAME_SIZE];
 uint8_t recv_buffer[ARRAY_FRAME_SIZE + 8 + 6];
 #define FRAME_TIME_US 33366
+
+#define TEXT_MOD_COLS 64
+#define TEXT_MOD_ROWS 48
+#include "font_t.h"
+uint32_t text_x = 0;
+uint32_t text_y = 0;
+uint32_t p10_font_rows = teletext[0];
+uint32_t p10_font_cols = teletext[1];
+
+void put_pixel(uint32_t x, uint32_t y, uint8_t color)
+{
+    if (x >= ARRAY_WIDTH_P10 * P10_COLS_PER_ROW * 8 || y >= ARRAY_HEIGHT_P10 * P10_HEIGHT)
+        return;
+    uint32_t idx = y * ARRAY_WIDTH_P10 * P10_COLS_PER_ROW * 8 + x;
+    recv_buffer[8 + 1 + idx / 8] |= (color << (7 - (idx % 8)));
+}
+
+void p10_handle_scroll()
+{
+    uint8_t *render_buf = recv_buffer + 8 + 1;
+    if (text_y >= TEXT_MOD_ROWS)
+    {
+        memmove_fast((void *)render_buf, (void *)(render_buf + p10_font_rows * TEXT_MOD_COLS / 8), (TEXT_MOD_ROWS - p10_font_rows) * TEXT_MOD_COLS / 8);
+        // clear bottom font_rows pixels
+        // for (int i = 480 - font_rows; i < 480; i++)
+        //     for (int j = 0; j < pitch / 4; j++)
+        //         fb[i * (pitch / 4) + j] = 0;
+        memset_fast((void *)(render_buf + (TEXT_MOD_ROWS - p10_font_rows) * TEXT_MOD_COLS / 8), 0, p10_font_rows * TEXT_MOD_COLS / 8);
+        text_y -= p10_font_rows;
+    }
+}
+
+void text_mode_render(char *str)
+{
+    uint8_t *render_buf = recv_buffer + 8 + 1;
+    while (*str)
+    {
+        if ((*str < 0x20 || *str > 0x7E) && *str != '\n')
+        {
+            printk("Unsupported character '%x' in printToScreen\n", *str);
+            str++;
+            continue;
+        }
+        if (*str == '\n') // just do the \r\n thingy
+        {
+            text_y += p10_font_rows;
+            text_x = 0;
+            p10_handle_scroll();
+        }
+        else
+        {
+            for (uint32_t r = 0; r < p10_font_rows; r++)
+                for (uint32_t c = 0; c < p10_font_cols; c++)
+                {
+                    uint8_t pixel_on = teletext[2 + (*str - 0x20) * p10_font_rows + r] & (1 << (p10_font_cols - 1 - c));
+                    put_pixel(text_x + c, text_y + r, pixel_on ? 1 : 0);
+                }
+            text_x += p10_font_cols;
+        }
+        if (text_x >= TEXT_MOD_COLS)
+        {
+            text_x = 0;
+            text_y += p10_font_rows;
+            p10_handle_scroll();
+        }
+        str++;
+    }
+    单接口单向一路带数据(recv_buffer + 8 + 1, frame_buffer);
+}
+
 void p10_start_render_loop()
 {
     while (1)
@@ -117,9 +187,11 @@ void p10_start_render_loop()
 }
 void p10_start_server()
 {
+    memset(frame_buffer, 0xff, sizeof(frame_buffer));
     if (w5500_udp_open(1, 9800) < 0)
     {
         printk("[p10] UDP socket open failed\n");
+        text_mode_render("+p10+ UDP socket open failed\n");
         return;
     }
     gpio_set_output(OE);
@@ -127,7 +199,16 @@ void p10_start_server()
     gpio_set_output(ADD_B);
     gpio_set_output(SCLK);
     printk("[p10] UDP socket opened\n");
-    memset(frame_buffer, 0xff, sizeof(frame_buffer));
+    text_mode_render("+p10+ UDP socket opened\n");
+    char ip_str[16];
+    w5500_get_ip(ip_str);
+    text_mode_render(ip_str);
+    w5500_get_subnet(ip_str);
+    text_mode_render("\n");
+    text_mode_render(ip_str);
+    w5500_get_gateway(ip_str);
+    text_mode_render("\n");
+    text_mode_render(ip_str);
     while (1)
     {
         rpi_yield();
